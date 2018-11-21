03e0b568a20498724a2ad8919177a777c655bafc
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-11-21 09:19:38 -0500

    Finished merge code.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/mergeSortArray.java
+++ src/mergeSortArray.java
@@ -20,9 +20,42 @@ public class mergeSortArray
 
     private void merge(int[] arr, int left, int mid, int right, int[] temp)
     {
-        for(int i = 0;i<right;i++)
-        {
+        int i = left;
+        int j = mid+1;
+        int k = left;
 
+        while(i<=mid && j<=right)
+        {
+            if(arr[i]<arr[j])
+            {
+                temp[k] = arr[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<=mid)
+        {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while(j<=right)
+        {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for(int a = left;a<=right;a++)
+        {
+            arr[a] = temp[a];
         }
     }
 }
